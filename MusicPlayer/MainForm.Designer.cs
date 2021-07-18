7f00def0b61e8973efde43e75b5c8c2fe0f210b4
GOJABoop <josealbertogarcia453@gmail.com>
2021-07-18 15:53:46 -0500

    Selecting files and playing them

---
+++ MusicPlayer/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alberto
+ * Date: 06/07/2021
+ * Time: 11:02 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace MusicPlayer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.OpenFileDialog openFileDialogFile;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.openFileDialogFile = new System.Windows.Forms.OpenFileDialog();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listBoxSongs = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialogFile
+            // 
+            this.openFileDialogFile.FileName = "openFileDialog1";
+            this.openFileDialogFile.Multiselect = true;
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(190, 12);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(363, 62);
+            this.player.TabIndex = 3;
+            // 
+            // listBoxSongs
+            // 
+            this.listBoxSongs.FormattingEnabled = true;
+            this.listBoxSongs.Location = new System.Drawing.Point(6, 12);
+            this.listBoxSongs.Name = "listBoxSongs";
+            this.listBoxSongs.Size = new System.Drawing.Size(178, 212);
+            this.listBoxSongs.TabIndex = 4;
+            this.listBoxSongs.SelectedIndexChanged += new System.EventHandler(this.listBoxSongs_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 231);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 279);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBoxSongs);
+            this.Controls.Add(this.player);
+            this.Name = "MainForm";
+            this.Text = "MusicPlayer";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.ListBox listBoxSongs;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ MusicPlayer/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+	public partial class MainForm : Form
+	{
+		string[] files;
+		string[] filesPaths;
+		Boolean play;
+
+		public MainForm(){
+			InitializeComponent();
+			play = true;
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e){
+			if(openFileDialogFile.ShowDialog() == System.Windows.Forms.DialogResult.OK){
+				files = openFileDialogFile.SafeFileNames;
+				filesPaths = openFileDialogFile.FileNames;
+				foreach(var file in files){
+					listBoxSongs.Items.Add(file);
+				}
+				player.URL = filesPaths[0];
+				listBoxSongs.SelectedIndex = 0;
+			}
+		}
+
+		private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e){
+			player.URL = filesPaths[listBoxSongs.SelectedIndex];
+		}
+	}
+}
